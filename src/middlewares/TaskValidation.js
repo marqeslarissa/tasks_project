53d2eb3024be836ee1a960bf967f24e971b2b3df
Larissa Marques <larissamarques@live.com.pt>
2021-12-29 19:13:52 +0000

    create validation

--- src/middlewares/TaskValidation.js
+++ src/middlewares/TaskValidation.js
@@ -1,17 +1,34 @@
 const TaskModel = require('../model/TaskModel');
-
+const { isPast } = require('date-fns');  //verificar se data ta no passado
 
 const TaskValidation = async (req, res, next) => {
   const { macaddress, type, title, description, when } = req.body;
 
-  if(!macaddress)
-    return res.status(400).json({error: 'Macaddress é obrigatório'});
-  else if(!type)
-   return res.status(400).json({error: 'Tipo é obrigatório'});
-   else if(!title)
-   return res.status(400).json({error: 'Título é obrigatório'});
-   else if(!description)
-   return res.status(400).json({error: 'Descrição é obrigatório'});
+  if (!macaddress)
+    return res.status(400).json({ error: 'Macaddress é obrigatório' });
+  else if (!type)
+    return res.status(400).json({ error: 'Tipo é obrigatório' });
+  else if (!title)
+    return res.status(400).json({ error: 'Título é obrigatório' });
+  else if (!description)
+    return res.status(400).json({ error: 'Descrição é obrigatório' });
+  else if (!when)
+    return res.status(400).json({ error: 'Data é obrigatório' });
+  else if (isPast(new Date(when)))
+    return res.status(400).json({ error: 'Coloque a data no presente ou futuro' });
+  else {
+    let exists;
+
+    exists = await TaskModel.
+      findOne
+      ({
+        'when': { '$eq': new Date(when) },  //$eq = igual ao
+        'macaddress': { '$in': macaddress } // $in = se existe
+      });
+    if (exists) {
+      return res.status(400).json({ error: 'Já existe uma tarefa nesse dia e horario' });
+    }
     next();
+  }
 }
 module.exports = TaskValidation;
--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -1,4 +1,4 @@
-const express = requise('express');
+const express = require('express');
 const router = express.Router();
 
 const TaskController = require('../controller/TaskController');
